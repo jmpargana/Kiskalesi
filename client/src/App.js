b97e28df6e94e3d0c80b8178be26b52ff3ebcffe
jmpargana <jmpargana@stud.uni-frankfurt.de>
2019-12-15 19:16:34 +0100

    auth0 authentication persistent

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Container from 'react-bootstrap/Container';
 
 import {
   Route,
-  BrowserRouter as Router,
-  Switch
+  Switch,
+  withRouter,
 } from 'react-router-dom';
 
 import Agenda from './components/Agenda';
@@ -23,15 +23,36 @@ import AppFooter from './components/AppFooter';
 import NavBar from './components/NavBar';
 import Callback from './components/Callback';
 import SecuredRoute from './components/SecuredRoute';
+import auth0Client from './components/Auth';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({checkingSession: false});
+      return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({checkingSession: false});
+  }
+
+  render() {
+    return (
+      <div className="App">
         <NavBar />
         <Container>
-          <div style={{height: 100 + "px"}}></div>
+          <div style={{height: 100 + 'px'}}></div>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/agenda/:eventId?" component={Agenda} />
@@ -39,15 +60,18 @@ function App() {
             <Route path="/explore/:eventId?" component={Explore} />
             <Route path="/infos/:eventId?" component={Infos} />
             <Route path="/location/:eventId" component={AppEvent} />
-            <Route exact path='/callback' component={Callback} /> 
-            <SecuredRoute path='/admin' component={CreateEvent} />
+            <Route exact path="/callback" component={Callback} />
+            <SecuredRoute path="/admin" 
+                          component={CreateEvent} 
+                          checkingSession={this.state.checkingSession}
+            />
             <Route component={NotFound} />
           </Switch>
-       </Container>
-      </Router>
+        </Container>
       <AppFooter />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
